11c6038e4be18f3733fb5b7112a693e136096b33
ckp8 <charanjit127@hotmail.com>
2020-12-02 10:33:22 +0000

    app

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+        <div id = "about">
+            <h2> About us </h2>
+            <p>Welcome to github 2.0 ,where you can search, discover and ......dhashd</p>
+
+
+
+
+
+            </div>
+
+
+    );
+}
+export default About;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import About from './About';
+
+const Welcome = () => {
+    return (
+        <div id = "welcome">
+            <h2> Welcome </h2>
+            <p>Hi, Welcome to our site.
+            </p>
+            <p>On the search bar at the top, you can </p>
+
+
+
+
+
+
+        </div>
+    )
+}
+export default About
