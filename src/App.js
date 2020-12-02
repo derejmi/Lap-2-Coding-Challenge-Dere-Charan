1a7b2343fcd2b1c14c2f75f0a2293e4a9b73e432
ckp8 <charanjit127@hotmail.com>
2020-12-02 10:53:29 +0000

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Form from "./components/Form";
+import Navbarr from "./components/Navbarr";
+import "bootstrap/dist/css/bootstrap.css";
+import About from "./pages/About";
+
 
 class App extends React.Component {
   state = {};
 
   render() {
     return (
+     
       <div>
-        <Form />
+        {/* <Form /> */}
+        <Navbarr/>
       </div>
     );
   }
