d540a33a7f583bd116d07dbfbb1eb86f05fd2beb
derejmi <derejemireyigbe@gmail.com>
2020-12-26 15:50:54 +0000

    add edit and gif

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Welcome from './Welcome';
-import './welcome.css'
+import "./welcome.css";
 
 class Welcome extends Component {
- render () {  return (
-        <div id = "Welcome">
-            <header class = "jumbotron">
-            <h2> Github 2.0 ❤️️ </h2>
-            </header>
-            <p>Hi ❤️️ ❤️️
-            </p>
-            <p>Welcome to github 2.0, please search your github username above ❤️️ ❤️️  </p>
-            <p>You will then be able to retrieve all your repositries and any data associated with those repos ❤️️ </p>
-
-
-
-
-
-
-        </div>
-    )
-     
- }
-  
+  render() {
+    return (
+      <div id="Welcome">
+        <header class="jumbotron">
+          <h2> Github 2.0 ❤️️ </h2>
+        </header>
+        <p>Hi ❤️️</p>
+        <p>Welcome to github 2.0, please search your github username above </p>
+        <p>
+          You will then be able to retrieve all your repositries and any data
+          associated with those repos{" "}
+        </p>
+      </div>
+    );
+  }
 }
 export default Welcome;
